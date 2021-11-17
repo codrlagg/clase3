647ac0a83b0b17c498a5e728a2222d6b12bcb928
luis garcia <luigy-antoni@outlook.com>
2021-11-17 07:28:50 -0600

    Entregable clase 3

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 let entrada = prompt("Adivina un número entre 0 y 50");
 while (entrada != 25) {
-    let intentos = entrada;
-    let intentos2 = intentos + "," + entrada;
+    console.log(entrada);
     alert("número  " + entrada + " es incorrecto");
     entrada = prompt ("Intenta con otro número entre 0 y 100");
-    alert(intentos2);
 }
+alert("felicidades el número buscado es 25");
